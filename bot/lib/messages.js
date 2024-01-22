3bb11175fd6b4941830e28515a97b1573705af5c
bekzod600 <bekzodbaratov18@gmail.com>
2024-01-22 12:50:30 +0500

    ozgartirildi

--- bot/lib/messages.js
+++ bot/lib/messages.js
@@ -1,5 +1,5 @@
 const messages = {
-  start: "<b>Let's get started 🍰</b> \n\nPlease tap the button below to order your perfect lunch!",
+  start: "<b>Let's get started 🍰</b> \n\nPlease tap the button below to order your perfect lunch...",
 };
 
 module.exports = { messages };
