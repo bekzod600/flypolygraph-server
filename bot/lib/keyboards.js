bcb6d48506d5587a1bdaa89d2c0cad4dcebffa98
bekzod600 <bekzodbaratov334@gmail.com>
2024-02-01 17:18:12 +0500

    web link changed

--- bot/lib/keyboards.js
+++ bot/lib/keyboards.js
@@ -1,8 +1,10 @@
 const { Markup } = require("telegraf");
-const web_link = "https://sq-market-client.vercel.app/";
+const web_link = process.env.WEBAPP_LINK;
 
 const keyboards = {
-  start: Markup.keyboard([[Markup.button.webApp("Order Food", web_link)]]).resize(),
+  start: Markup.keyboard([
+    [Markup.button.webApp("Order Food", web_link)],
+  ]).resize(),
 };
 
 module.exports = { keyboards };
